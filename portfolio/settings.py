2c2080233ab71a6b6da53b4a1f2fcd0783499475
Pathan Ilyas <ilyaskhanik1325@gmail.com>
2026-06-04 12:08:34 +0530

    Fix django-cloudinary-storage compatibility

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -276,6 +276,9 @@ STORAGES = {
     },
 }
 
+# Backward compatibility for django-cloudinary-storage which relies on deprecated settings
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
